919d7ae0dd2ef69ac7b3a2133969a05add391751
thomasfan <thomasfan@tencent.com>
2017-08-09 15:24:04 +0800

    添加注释和调试日志。

--- coroutine.c
+++ coroutine.c
@@ -17,24 +17,26 @@
 
 struct coroutine;
 
+// 调度器
 struct schedule {
-	char stack[STACK_SIZE];
-	ucontext_t main;
-	int nco;
-	int cap;
-	int running;
-	struct coroutine **co;
+	char stack[STACK_SIZE];    // 所有协程的public stack
+	ucontext_t main;           // 主线程的context
+	int nco;                   // 当前启用的协程数量
+	int cap;                   // 支持的协程数量
+	int running;               // 协程id
+	struct coroutine **co;     // 协程对象集
 };
 
+// 协程对象
 struct coroutine {
-	coroutine_func func;
-	void *ud;
-	ucontext_t ctx;
-	struct schedule * sch;
-	ptrdiff_t cap;
-	ptrdiff_t size;
-	int status;
-	char *stack;
+	coroutine_func func;       // 每个协程的回调函数
+	void *ud;                  // 每个协程的用户数据
+	ucontext_t ctx;            // 每个协程的context
+	struct schedule * sch;     // 每个协程从属的调度器
+	ptrdiff_t cap;             // 每个协程private stack的最大分配空间
+	ptrdiff_t size;            // 每个协程private stack的实际分配空间
+	int status;                // 每个协程的当前运行状态
+	char *stack;               // 协程的private stack
 };
 
 struct coroutine * 
@@ -52,6 +54,7 @@ _co_new(struct schedule *S , coroutine_func func, void *ud) {
 
 void
 _co_delete(struct coroutine *co) {
+    // 只释放协程自己动态分配的空间, 并注意释放顺序
 	free(co->stack);
 	free(co);
 }
@@ -76,6 +79,8 @@ coroutine_close(struct schedule *S) {
 			_co_delete(co);
 		}
 	}
+
+    // 最后释放调度器
 	free(S->co);
 	S->co = NULL;
 	free(S);
@@ -99,7 +104,7 @@ coroutine_new(struct schedule *S, coroutine_func func, void *ud) {
 			if (S->co[id] == NULL) {
 				S->co[id] = co;
 				++S->nco;
-				return id;
+				return id; // 返回创建好的协程id
 			}
 		}
 	}
@@ -109,9 +114,11 @@ coroutine_new(struct schedule *S, coroutine_func func, void *ud) {
 
 static void
 mainfunc(uint32_t low32, uint32_t hi32) {
+    // resume param
 	uintptr_t ptr = (uintptr_t)low32 | ((uintptr_t)hi32 << 32);
 	struct schedule *S = (struct schedule *)ptr;
 	int id = S->running;
+    printf("mainfunc: coroutine id[%d]\n", S->running);
 	struct coroutine *C = S->co[id];
 	C->func(S,C->ud);
 	_co_delete(C);
@@ -131,20 +138,27 @@ coroutine_resume(struct schedule * S, int id) {
 	switch(status) {
 	case COROUTINE_READY:
 		getcontext(&C->ctx);
-		C->ctx.uc_stack.ss_sp = S->stack;
-		C->ctx.uc_stack.ss_size = STACK_SIZE;
-		C->ctx.uc_link = &S->main;
-		S->running = id;
-		C->status = COROUTINE_RUNNING;
-		uintptr_t ptr = (uintptr_t)S;
-		makecontext(&C->ctx, (void (*)(void)) mainfunc, 2, (uint32_t)ptr, (uint32_t)(ptr>>32));
+		C->ctx.uc_stack.ss_sp = S->stack;     // stack top起始位置(SP)
+		C->ctx.uc_stack.ss_size = STACK_SIZE; // 用于计算stack bottom(数据从stack bottom开始存放)
+		C->ctx.uc_link = &S->main;            // 协程执行完切回的context
+		S->running = id;                      // 调度器记录当前准备调度的协程id
+		C->status = COROUTINE_RUNNING;        // 将准备调度的协程状态置为"待运行状态"
+		uintptr_t ptr = (uintptr_t)S;         // 需要考虑跨平台指针大小不同的问题
+        // 开始执行mainfunc回调, 执行完继续fall through, 即执行下一个协程
+        makecontext(&C->ctx, (void (*)(void)) mainfunc, 2, (uint32_t)ptr, (uint32_t)(ptr>>32));
 		swapcontext(&S->main, &C->ctx);
+        printf("COROUTINE_READY: coroutine id[%d] return\n", S->running);
 		break;
 	case COROUTINE_SUSPEND:
+        // stack从高地址向低地址生长, 即从stack bottom向stack top存储数据
 		memcpy(S->stack + STACK_SIZE - C->size, C->stack, C->size);
 		S->running = id;
 		C->status = COROUTINE_RUNNING;
 		swapcontext(&S->main, &C->ctx);
+        // <debug>
+        sleep(2);
+        printf("COROUTINE_SUSPEND: coroutine id[%d] return\n", S->running);
+        // </debug>
 		break;
 	default:
 		assert(0);
@@ -153,8 +167,13 @@ coroutine_resume(struct schedule * S, int id) {
 
 static void
 _save_stack(struct coroutine *C, char *top) {
+    // 在stack上创建一个局部变量, 标识当前栈顶(SP)的位置(低地址)
 	char dummy = 0;
-	assert(top - &dummy <= STACK_SIZE);
+    printf("_save_stack: &C[%p] top[%p] &dummy[%p] top - &dummy[%d] STACK_SIZE[%d]\n", &C, top, &dummy, top - &dummy, STACK_SIZE);
+	// 检查stack是否有溢出
+    assert(top - &dummy <= STACK_SIZE);
+    // 按需保存当前协程的stack (begin)
+    // 判断协程栈的空间是否足够, 若不够则重新分配
 	if (C->cap < top - &dummy) {
 		free(C->stack);
 		C->cap = top-&dummy;
@@ -162,14 +181,20 @@ _save_stack(struct coroutine *C, char *top) {
 	}
 	C->size = top - &dummy;
 	memcpy(C->stack, &dummy, C->size);
+    // 按需保存当前协程的stack (end)
 }
 
 void
 coroutine_yield(struct schedule * S) {
 	int id = S->running;
+    printf("coroutine_yield: coroutine id[%d] into\n", S->running);
 	assert(id >= 0);
+    // 注意makecontext()时指定ss_sp为S->stack, 当C->ctx执行时, 协程的栈是存储在S->stack上的, 即把堆上分配的一块空间虚拟成栈来使用
 	struct coroutine * C = S->co[id];
+    // 与栈顶S->stack的位置进行比较
+    printf("coroutine_yield: &C[%p] S->stack[%p]\n", &C, S->stack);
 	assert((char *)&C > S->stack);
+    // 按需动态保存协程的private stack
 	_save_stack(C,S->stack + STACK_SIZE);
 	C->status = COROUTINE_SUSPEND;
 	S->running = -1;
